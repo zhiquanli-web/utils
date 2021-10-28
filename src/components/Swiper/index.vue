b6b452b119b17317685dc82066c09487e335e74a
lizhiquan <lizhiquan@tuzhanai.com>
2021-10-28 23:44:36 +0800

    滑动调试

--- src/components/Swiper/index.vue
+++ src/components/Swiper/index.vue
@@ -4,12 +4,21 @@
     <div
       class="swiper-container"
       ref="swiper-container"
-      @touchend="touchend"
-      @touchmove="touchmove"
-      @touchstart="touchstart"
+      @mouseup="mouseup"
+      @mousemove="mousemove"
+      @mousedown="mousedown"
+      @mouseout="mouseout"
+      @mouseover="mouseover"
     >
       <!-- 滑块 -->
-      <div class="swiper-item" v-for="item in data" :key="item">{{ item }}</div>
+      <div
+        class="swiper-item"
+        ref="swiper-item"
+        v-for="item in data"
+        :key="item"
+      >
+        {{ item }}
+      </div>
     </div>
   </div>
 </template>
@@ -20,43 +29,83 @@ export default {
   data() {
     return {
       data: [1, 2, 3, 4],
-      curIndex: 0,
-      startX: 0,
-      distance: 0,
-      sumWidth: 0,
+      index: 0, // 当前滑块的索引值
+      status: 0, // 状态值，是否跟随鼠标移动
+      oldX: 0, // 起点坐标
+      newX: 0, // 新坐标
     };
   },
   mounted() {
+    // 滑动容器
     this.swiper = this.$refs["swiper-container"];
+    this.itemContainer = this.$refs["swiper-item"]; // 滑动块
+    this.itemWidth = this.itemContainer[0].offsetWidth; // 单个滑块的宽度
+    this.left = 0 - this.itemWidth * this.index; // 容器的出事left值
+    // 设置容器的初始位置
+    this.swiper.style.left = this.left + "px";
+    this.autoMove();
   },
   methods: {
-    touchend() {
-      if (this.distance > 50) {
-        console.log("rtl");
-        this.movePage("rtl");
-      } else if (this.distance < -50) {
-        console.log("ltr");
-        this.movePage("ltr");
-      }
+    mousedown(e) {
+      this.status = 1;
+      this.oldX = this.startX = e.pageX; // 开始坐标
     },
-    touchmove(e) {
+    mousemove(e) {
       if (e && e.preventDefault) {
         e.preventDefault();
       }
-      this.distance = this.startX - e.changedTouches[0].pageX;
-    },
-    touchstart(e) {
-      this.startX = e.touches[0].pageX;
-    },
-    movePage(type, width = 300) {
-      if (type === "rtl" && this.curIndex < this.data.length - 1) {
-        this.curIndex += 1;
+      if (this.status !== 1) return;
+      this.newX = e.pageX;
+      if (this.newX < this.oldX) {
+        this.left -= this.oldX - this.newX;
+      } else {
+        this.left += this.newX - this.oldX;
       }
-      if (type === "ltr" && this.curIndex > 0) {
-        this.curIndex -= 1;
+      this.oldX = this.newX;
+      this.swiper.style.left = this.left + "px";
+    },
+    mouseup(e) {
+      this.status = 0;
+      this.movePage(e.pageX);
+    },
+    mouseout() {
+      this.autoMove();
+    },
+    movePage(width) {
+      if (width < this.startX) {
+        this.index++;
+      } else {
+        this.index--;
       }
-      this.sumWidth = -this.curIndex * width;
-      this.swiper.style.left = this.sumWidth + "px";
+      if (this.index < 0) {
+        this.index = 0;
+      } else if (this.index > this.itemContainer.length - 1) {
+        this.index = this.itemContainer.length - 1;
+      }
+      this.swiper.className += " move";
+      this.swiper.addEventListener("transitionend", () => {
+        this.swiper.className = this.swiper.className.replace(/\s+move/, "");
+      });
+      this.left = 0 - this.itemWidth * this.index;
+      this.swiper.style.left = this.left + "px";
+    },
+    autoMove() {
+      this.timer = setInterval(() => {
+        this.index++;
+        if (this.index > this.itemContainer.length - 1) {
+          this.index = 0;
+        }
+        this.swiper.className += " move";
+        this.swiper.addEventListener("transitionend", () => {
+          this.swiper.className = this.swiper.className.replace(/\s+move/, "");
+        });
+
+        this.left = 0 - this.itemWidth * this.index;
+        this.swiper.style.left = this.left + "px";
+      }, 2000);
+    },
+    mouseover() {
+      clearInterval(this.timer);
     },
   },
 };
@@ -72,12 +121,14 @@ export default {
     position: relative;
     display: flex;
     left: 0px;
-    transition: all 0.5s linear;
     .swiper-item {
       flex-shrink: 0;
       width: 300px;
       height: 300px;
       background: #eee;
+    }
+    &.move {
+      transition: all 0.2s ease-in-out;
     }
   }
 }
