175437e428034d77eb58f4e631d303b701050304
lizhiquan <lizhiquan>
2022-06-09 22:41:04 +0800

    feat: bind函数实现

--- utils/hyBind.js
+++ utils/hyBind.js
@@ -5,6 +5,30 @@
  * @Last Modified time: 2022-06-Th 06:38:32 
  */
 
-function hyBind(fn, context) {
-  
+Function.prototype.hyBind = function(thisArg, ...argsArray) {
+  var fn = this;
+  thisArg = thisArg !== null && thisArg !== undefined ? Object(thisArg) : window;
+  function proxyFn(...args) {
+    thisArg.fn = fn
+    var finalArgs = [...argsArray, ...args];
+    var result = thisArg.fn(...finalArgs);
+    delete thisArg.fn;
+    return result
+  }
+  return proxyFn
 }
+
+
+// test
+const obj = {
+  name: "lizhiquan",
+  age: 18,
+}
+
+function test(name, age) {
+  console.log(this.name, this.age);
+}
+
+
+const testFn = test.hyBind(obj, "zhangsan", 24);
+testFn()
--- utils/index.js
+++ utils/index.js
@@ -1,22 +1,19 @@
-let arr = [1,2,3,4]
+let arr = [1, 2, 3, 4];
 const result = arr.reduceRight((acc, item) => {
   return acc + item;
-}, 0)
+}, 0);
 
-console.log('result', result);
-
+console.log("result", result);
 
 function hyFlat(arr) {
-  const isDeep = arr.some(item => item instanceof Array)
-  if (!isDeep) return arr
-  const result = Array.prototype.concat.apply([], arr)
-  return result
+  const isDeep = arr.some((item) => item instanceof Array);
+  if (!isDeep) return arr;
+  const result = Array.prototype.concat.apply([], arr);
+  return result;
 }
 
-
-
 function hyDeepClone(obj) {
-  if (obj === null || typeof obj !== 'object') {
+  if (obj === null || typeof obj !== "object") {
     return obj;
   }
   const newObj = obj instanceof Array ? [] : {};
@@ -26,4 +23,34 @@ function hyDeepClone(obj) {
     }
   }
   return newObj;
+}
+
+function hyBind(thisArg, ...args) {
+  const fn = this;
+  thisArg =
+    thisArg !== null && thisArg !== undefined ? Object(thisArg) : window;
+  function proxyFn(...arg2s) {
+    thisArg.fn = fn;
+    return thisArg.fn([...args, ...arg2s]);
+  }
+  return proxyFn;
+}
+
+function hyCall(thisArg, ...args) {
+  const fn = this
+  thisArg = thisArg !== null && thisArg !== undefined ? Object(thisArg) : window
+  thisArg.fn = fn
+  const result = thisArg.fn(...args)
+  delete thisArg.fn
+  return result
+}
+
+
+function hyApply(thisArg, args = []) {
+  const fn = this
+  thisArg = thisArg !== null && thisArg !==undefined ? Object(thisArg) : window
+  thisArg.fn  = fn
+  const result  = thisArg.fn(...args)
+  delete thisArg.fn
+  return result
 }
